6f58693f624cc931ad5810dc8ec58f1f7be8cd1c
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-06-04 21:32:59 +0300

    1

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://683e07251cd60dca33da39bf.mockapi.io";
-
 export const fetchContacts = createAsyncThunk(
   "contacts/fetchAll",
   async (_, thunkAPI) => {
